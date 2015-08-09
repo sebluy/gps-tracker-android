ea55d08e810d9f45080ab578464ff965db99be9a
Sebastian Luy <sebastian.a.luy@gmail.com>
2015-08-09 10:12:55 -0400

    Add primitive gps tracking capabilities

--- app/src/main/java/android/sebluy/gpstracker/MainActivity.java
+++ app/src/main/java/android/sebluy/gpstracker/MainActivity.java
@@ -1,7 +1,7 @@
 package android.sebluy.gpstracker;
 
 import android.content.Context;
-import android.content.DialogInterface;
+import android.location.Location;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
@@ -13,28 +13,78 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationRequest;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.BufferedOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.DateFormat;
+import java.util.Date;
 
-public class MainActivity extends Activity {
+public class MainActivity
+        extends Activity
+        implements ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
 
     private String mStatus;
     private TextView mStatusView;
+    private TextView mLatitudeView;
+    private TextView mLongitudeView;
+    private TextView mTimeView;
+    private TextView mPointCountView;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private boolean mIsTracking = false;
 
     private void handleSendClick() {
         ConnectivityManager c = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo n = c.getActiveNetworkInfo();
         if (n != null && n.isConnected()) {
-            mStatusView.setText("Sending");
+            mStatus = "Sending";
+            updateStatusView();
             new SendPathTask().execute();
         } else {
-            mStatusView.setText("Network connection unavailable");
+            mStatus = "Network connection unavailable";
+            updateStatusView();
         }
+    }
+
+    private void updateStatusView() {
+        mStatusView.setText(mStatus);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mStatus = "Google API Connected";
+        updateStatusView();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        mStatus = "Google API Disconnected";
+        updateStatusView();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        mStatus = "Google API Connection Failed";
+        updateStatusView();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLatitudeView.setText(String.valueOf(location.getLatitude()));
+        mLongitudeView.setText(String.valueOf(location.getLongitude()));
+        mTimeView.setText(DateFormat.getTimeInstance().format(new Date()));
     }
 
     private class SendPathTask extends AsyncTask<Void, Void, Void> {
@@ -54,13 +104,34 @@ public class MainActivity extends Activity {
             mStatusView.setText(mStatus);
         }
 
+        private JSONObject latLngToJSON(double latitude, double longitude) throws Exception {
+            JSONObject latLng = new JSONObject();
+            latLng.put("latitude", latitude);
+            latLng.put("longitude", longitude);
+            return latLng;
+        }
+
+        private JSONArray makeJSONAPICall(String action, JSONArray args) {
+            JSONArray apiCall = new JSONArray();
+            apiCall.put(action);
+            apiCall.put(args);
+            return apiCall;
+        }
+
         private void sendPath() throws Exception {
             URL url = null;
             HttpURLConnection c = null;
             OutputStream os = null;
-            String apiCall = "[[\"add-path\", [{\"latitude\": 43.2, \"longitude\": -70.0}," +
-                        "{\"latitude\": 43.3, \"longitude\": -70.0}]]]";
-            byte[] bytes = apiCall.getBytes();
+
+            JSONArray path = new JSONArray();
+            path.put(latLngToJSON(43.2, -70.0)) ;
+            path.put(latLngToJSON(43.3, -70.0)) ;
+
+            JSONArray apiCall = makeJSONAPICall("add-path", path);
+            JSONArray apiCallList = new JSONArray();
+            apiCallList.put(apiCall);
+
+            byte[] bytes = apiCallList.toString().getBytes();
             try {
                 url = new URL("https://fierce-dawn-3931.herokuapp.com/api");
                 c = (HttpURLConnection) url.openConnection();
@@ -92,6 +163,24 @@ public class MainActivity extends Activity {
         }
     }
 
+    private void buildGoogleApiClient() {
+        mStatus = "Connecting";
+        updateStatusView();
+        mGoogleApiClient = new GoogleApiClient.Builder(this).
+                addConnectionCallbacks(this).
+                addOnConnectionFailedListener(this).
+                addApi(LocationServices.API).
+                build();
+        createLocationRequest();
+    }
+
+    private void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -103,6 +192,54 @@ public class MainActivity extends Activity {
                 handleSendClick();
             }
         });
+        Button startTrackingButton = (Button)findViewById(R.id.start_tracking_button);
+        startTrackingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!mIsTracking && mGoogleApiClient.isConnected()) {
+                    startLocationUpdates();
+                    mIsTracking = true;
+                }
+            }
+        });
+        Button stopTrackingButton = (Button)findViewById(R.id.stop_tracking_button);
+        stopTrackingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mIsTracking) {
+                    stopLocationUpdates();
+                    mIsTracking = false;
+                }
+                
+            }
+        });
+        mLatitudeView = (TextView)findViewById(R.id.latitude);
+        mLongitudeView = (TextView)findViewById(R.id.longitude);
+        mTimeView = (TextView)findViewById(R.id.time);
+        mPointCountView = (TextView)findViewById(R.id.point_count);
+        buildGoogleApiClient();
+    }
+
+    private void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+    }
+
+    private void startLocationUpdates() {
+        LocationServices.FusedLocationApi.
+                requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onStop() {
+        stopLocationUpdates();
+        mGoogleApiClient.disconnect();
+        super.onStop();
     }
 
     @Override
