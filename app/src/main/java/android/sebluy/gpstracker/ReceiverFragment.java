f1e5c04e15b1bcad86191d24a32cc33285353b4b
Sebastian Luy <sebastian.a.luy@gmail.com>
2015-08-16 18:03:41 -0400

    Log data from bluetooth connection

--- app/src/main/java/android/sebluy/gpstracker/ReceiverFragment.java
+++ app/src/main/java/android/sebluy/gpstracker/ReceiverFragment.java
@@ -4,26 +4,40 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothManager;
 import android.bluetooth.BluetoothAdapter.LeScanCallback;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
 
 public class ReceiverFragment extends DialogFragment {
 
+    private static final String TAG = ReceiverFragment.class.getSimpleName();
+
     private static final long SCAN_PERIOD = 10000;
+
+    private static final UUID CCCD = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb");
+    private static final UUID RX_SERVICE_UUID = UUID.fromString("6e400001-b5a3-f393-e0a9-e50e24dcca9e");
+    private static final UUID TX_CHARA_UUID = UUID.fromString("6e400003-b5a3-f393-e0a9-e50e24dcca9e");
 
     private BluetoothAdapter mBluetoothAdapter;
     private ArrayAdapter<String> mAdapter;
-    private ArrayList<String> mDevices;
+    private HashMap<String, BluetoothDevice> mDevices;
     private Handler mHandler;
 
     @Override
@@ -39,9 +53,9 @@ public class ReceiverFragment extends DialogFragment {
 
         mHandler = new Handler();
 
-        mDevices = new ArrayList<>();
+        mDevices = new HashMap<>();
         mAdapter = new ArrayAdapter<>(getActivity(),
-                android.R.layout.simple_list_item_1, mDevices);
+                android.R.layout.simple_list_item_1);
 
         startScan();
 
@@ -51,6 +65,9 @@ public class ReceiverFragment extends DialogFragment {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 stopScan();
+                                startLoading(mAdapter.getItem(which));
+                                ProgressDialog.show(getActivity(), "Loading", "Please wait",
+                                        true, true);
                             }
                         }).
                 setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -70,6 +87,47 @@ public class ReceiverFragment extends DialogFragment {
         }
     }
 
+    private void startLoading(String deviceName) {
+        BluetoothDevice device = mDevices.get(deviceName);
+        Log.d(TAG, "Connecting " + deviceName);
+        BluetoothGatt gatt = device.connectGatt(getActivity(), false, new BluetoothGattCallback() {
+            @Override
+            public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+                super.onConnectionStateChange(gatt, status, newState);
+                Log.d(TAG, "Connected");
+                if (newState == BluetoothProfile.STATE_CONNECTED) {
+                    gatt.discoverServices();
+                }
+            }
+
+            @Override
+            public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+                super.onServicesDiscovered(gatt, status);
+                Log.d(TAG, "Services discovered");
+                enableTxNotifications(gatt);
+            }
+
+            @Override
+            public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+                super.onCharacteristicChanged(gatt, characteristic);
+                try {
+                    Log.d(TAG, new String(characteristic.getValue(), "UTF-8"));
+                } catch (Exception e){
+                    Log.d(TAG, "Decoding failed");
+                }
+            }
+        });
+        gatt.getServices();
+    }
+
+    private void enableTxNotifications(BluetoothGatt gatt) {
+        BluetoothGattCharacteristic tx = gatt.getService(RX_SERVICE_UUID).getCharacteristic(TX_CHARA_UUID);
+        gatt.setCharacteristicNotification(tx, true);
+        BluetoothGattDescriptor descriptor = tx.getDescriptor(CCCD);
+        descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+        gatt.writeDescriptor(descriptor);
+    }
+
     private void startScan() {
         mHandler.postDelayed(new Runnable() {
             @Override
@@ -87,6 +145,7 @@ public class ReceiverFragment extends DialogFragment {
     private LeScanCallback mScanCallback = new LeScanCallback() {
         @Override
         public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
+            mDevices.put(device.getName(), device);
             mAdapter.add(device.getName());
             mAdapter.notifyDataSetChanged();
         }
